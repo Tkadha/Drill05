f750c6a0f6feab53d76c5e5aa4cb5a828602f385
Tkadha <emforhs228@naver.com>
2023-09-28 19:22:42 +0900

    틀생성

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -8,11 +8,16 @@ character = load_image('animation_sheet.png')
 hand = load_image('hand_arrow.png')
 
 
+def char_move():
+    pass
 
+
+while True:
+    clear_canvas()
+    TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    hand.draw()
+    char_move()
+    update_canvas()
 
 
 close_canvas()
-
-
-
-
