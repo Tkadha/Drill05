120cae11ed8442b2d51fcd3a91507113b189ba80
Tkadha <emforhs228@naver.com>
2023-09-28 19:18:03 +0900

    create drill05 file

---
+++ move_character_with_mouse.py
@@ -0,0 +1,18 @@
+from pico2d import *
+
+TUK_WIDTH, TUK_HEIGHT = 1240, 600
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+
+TUK_ground = load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.png')
+hand = load_image('hand_arrow.png')
+
+
+
+
+
+close_canvas()
+
+
+
+
